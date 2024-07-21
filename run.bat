ecb7132e7af1fdc9bea196bd8daf17d5722144bf
PrestonDragon <122578013+PrestonDragon989@users.noreply.github.com>
2024-07-21 13:38:32 -0700

    Update run.bat
    Fixed path

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 :: Check if the first argument is empty
 if "%~1"=="" (
-    python3 "C:\Passwords (Steg)\Converter\main.py"
+    python3 "%~dp0main.py"
 ) else (
     python3 "%~dp0main.py" "%~1"
 )
